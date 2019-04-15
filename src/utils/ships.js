a68d691e45e12c11578bd5d5ba086fba2131ff4d
Avonavi232 <Avonavi232@gmail.com>
2019-04-15 22:03:48 +0300

    Ship class updated:  * catchShoot method added. It will be called, when event from websocket comes (opponent did shot)  * when catchShoot triggers, it modifies the ship instance and calls the redux action in order to update UI

--- src/utils/ships.js
+++ src/utils/ships.js
@@ -1,9 +1,12 @@
 import uuid from "uuid/v4";
+import {isNumeric} from "./functions";
+import {updatePlayerShip} from "../actions";
 
 class BasicShip{
-    constructor(params) {
-        this.parts = [];
+    constructor(params, dispatch) {
         this.id = uuid();
+        this.parts = {};
+        this.dispatch = dispatch || Function.prototype;
 
         if (params) {
             this.pos = params;
@@ -11,14 +14,15 @@ class BasicShip{
     }
 
     set pos({x, y, direction}){
-        if (x && y && direction !== undefined) {
+        if (isNumeric(x) && isNumeric(y) && direction !== undefined) {
             this.start = {x, y};
             for (let i = 0; i < this.length; i++) {
-                this.parts.push({
-                    x: direction ? this.start.x : this.start.x + i,
-                    y: !direction ? this.start.y : this.start.y + i,
-                    id: `${this.id}_${i+1}`
-                })
+                const
+                    x = direction ? this.start.x : this.start.x + i,
+                    y = !direction ? this.start.y : this.start.y + i,
+                    id = `${this.id}_${i+1}`;
+
+                this.parts[`${x};${y}`] = { x, y, id, type: 'default' };
             }
         }
     }
@@ -26,6 +30,33 @@ class BasicShip{
     get length(){
         return 1;
     }
+
+    hasPart(x, y){
+        return this.parts.hasOwnProperty(`${x};${y}`);
+    }
+
+    isDie(){
+        for (let key in this.parts) {
+            if (this.parts[key].type !== 'shot') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    catchShoot = (x, y) => {
+        if (this.hasPart(x, y)) {
+            this.parts[`${x};${y}`].type = 'shot';
+
+            if (this.isDie()) {
+                for (let key in this.parts) {
+                    this.parts[key].type = 'die';
+                }
+            }
+
+            this.dispatch(updatePlayerShip(this));
+        }
+    };
 }
 
 export class SingleShip extends BasicShip{
