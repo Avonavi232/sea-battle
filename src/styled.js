14d51de0ad2e3a9985e87ce55360a25e9cc98c74
Avonavi232 <Avonavi232@gmail.com>
2019-04-18 18:49:35 +0300

    styled components added:  * new Cell types

--- src/styled.js
+++ src/styled.js
@@ -2,7 +2,10 @@ import React from 'react';
 import styled, {css, createGlobalStyle} from 'styled-components/macro';
 import {darken} from 'polished';
 
+import {ReactComponent as AimIcon} from './img/aim.svg';
+import {ReactComponent as HitIcon} from './img/hit.svg';
 import config from './config';
+
 
 export const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Roboto:300,400');
@@ -19,6 +22,7 @@ export const GlobalStyle = createGlobalStyle`
 
 const theme = {
     cellColor: '#bbe4f9',
+    inkColor: darken(0.3, '#bbe4f9'),
     cellLineWidth: '1px',
     boardFontSize: '36px',
     boardSize: config.boardSize
@@ -63,19 +67,13 @@ export const Grid = styled.div`
   place-content: start center;
 `;
 
-export const MoveTimer = styled.div`
-  grid-area: moveTimer;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 36px;
-  font-weight: 300;
-`;
-
 export const MoveIndicator = styled.div`
     grid-area: MoveIndicator;
     place-self: center;
     padding: 1rem;
+    transition: all .3s;
+    transform-origin: center;
+    transform: ${({shooter}) => !shooter && 'rotate(180deg)'};
     
     &:before {
         content: '';
@@ -113,6 +111,9 @@ export const OpponentBoard = styled.div`
   ${boardGrid}
 `;
 
+
+/************CELLS***********/
+
 const cellHoverBase = css`
     &:hover {
         cursor: pointer;      
@@ -135,11 +136,74 @@ export const HoverCell = styled(Cell)`
   }
 `;
 
+export const AimCell = styled(props => {
+    return (
+        <Cell {...props}>
+            <AimIcon className="aim-cell__svg"/>
+        </Cell>
+    )
+})`
+  ${cellHoverBase};
+  transition: all .1s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  .aim-cell__svg {
+    opacity: 0;
+    transition: all .2s;
+    width: 80%;
+    height: 80%;
+    display: block;
+    position: relative;
+    left: 1px;
+    top: 1px;
+    path{
+      fill: #35d494;
+    }
+  }
+  
+  &:hover .aim-cell__svg{
+    opacity: 1;
+    animation: heartbeat 1.5s ease-in-out infinite both;
+  }
+  
+    @keyframes heartbeat {
+      from {
+        transform: scale(1);
+        transform-origin: center center;
+        animation-timing-function: ease-out;
+      }
+      10% {
+        transform: scale(0.91);
+        animation-timing-function: ease-in;
+      }
+      17% {
+        transform: scale(0.98);
+        animation-timing-function: ease-out;
+      }
+      33% {
+        transform: scale(0.87);
+        animation-timing-function: ease-in;
+      }
+      45% {
+        transform: scale(1);
+        animation-timing-function: ease-out;
+      }
+    }
+`;
+
 export const ShipCell = styled(Cell)`
   background-color: #6a9cf8;
 `;
 
-export const ShipShotCell = styled(ShipCell)`
+export const ShotHitCell = styled(props => {
+    return (
+        <Cell {...props}>
+            <HitIcon className="hit-cell__svg"/>
+        </Cell>
+    )
+})`
   position: relative;
   overflow: hidden;
   &:after,
@@ -150,7 +214,7 @@ export const ShipShotCell = styled(ShipCell)`
     bottom: -25%;
     left: 50%;
     width: 2px;
-    background-color: red;
+    background-color: #ff6464;
   }
   &:before {
     transform: translateX(-50%) rotate(45deg);
@@ -158,10 +222,33 @@ export const ShipShotCell = styled(ShipCell)`
   &:after {
     transform: translateX(-50%) rotate(-45deg);
   }
+  .hit-cell__svg {
+    width: 100%;
+    height: 100%;
+    path {
+      fill: ${theme.inkColor}
+    }
+  }
 `;
 
-export const ShipDieCell = styled(ShipShotCell)`
+export const ShipDieCell = styled(ShotHitCell)`
   opacity: 0.3;
+`;
+
+export const ShotMissCell = styled(Cell)`
+  position: relative;
+  overflow: hidden;
+  &:before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: .3em;
+    height: .3em;
+    border-radius: 50%;
+    background-color: red;
+  }
 `;
 
 export const LetterCell = styled(Cell)`
@@ -169,7 +256,7 @@ export const LetterCell = styled(Cell)`
   align-items: center;
   justify-content: center;
   font-family: 'Patrick Hand', cursive;
-  color: ${darken(0.3, theme.cellColor)};
+  color: ${theme.inkColor};
   user-select: none;
   &:hover {
     cursor: default;
