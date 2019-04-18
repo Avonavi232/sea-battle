04fe0cb96bb8f134a6dec880b68b07f560854f38
Avonavi232 <Avonavi232@gmail.com>
2019-04-18 18:47:52 +0300

    Timer component added (countdown timer)

---
+++ src/components/Timer.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import styled from "styled-components";
+
+
+
+class Timer extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            deadline: moment.duration(this.props.deadline)
+        };
+
+        this.intervalID = null;
+    }
+
+    componentDidMount() {
+        this.init();
+    }
+
+    init(){
+        clearInterval(this.intervalID);
+        this.setState({
+            deadline: moment.duration(this.props.deadline)
+        });
+
+        this.intervalID = setInterval(() => {
+            const newDuration = this.state.deadline.clone();
+
+            if (this.state.deadline.seconds() > 0) {
+                newDuration.subtract(1, 's');
+            } else {
+                clearInterval(this.intervalID);
+                return;
+            }
+            this.setState({deadline: newDuration});
+        }, 1000);
+    }
+
+    render() {
+        const {deadline} = this.state;
+        return (
+            <div className={this.props.className}>
+                {deadline.minutes()}
+                :
+                {deadline.seconds()}
+            </div>
+        );
+    }
+}
+
+export const StyledTimer = styled(Timer)`
+  grid-area: moveTimer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 36px;
+  font-weight: 300;
+`;
+
+StyledTimer.propTypes = {
+    deadline: PropTypes.number.isRequired,
+};
+
+export default StyledTimer;
