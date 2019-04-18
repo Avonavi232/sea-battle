b7b06afc697df39a693c2a17ca28a4e37d44c459
Avonavi232 <Avonavi232@gmail.com>
2019-04-18 21:06:34 +0300

    RoomCreator refactor

--- src/components/RoomCreator.js
+++ src/components/RoomCreator.js
@@ -1,8 +1,11 @@
 import React, {Component} from 'react';
-
-//Redux
 import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+
 import {getDeepProp} from "../utils/functions";
+import {gameConnection} from "../utils/gameConnection";
+import {gameSides, gameStatuses} from "../utils/constants";
+import {setGameStatus, setRoomSettings} from "../actions";
 
 class RoomCreator extends Component {
     constructor(props) {
@@ -15,9 +18,26 @@ class RoomCreator extends Component {
 
     handleSubmit = event => {
         event.preventDefault();
-        this.props.createRoom(this.state)
-            .then(({roomID}) => this.props.connectToRoom({roomID, playerID: this.props.playerID}))
+        gameConnection.createRoom(this.state, this.props.playerID)
+            .then(roomID => gameConnection.connectToRoom({roomID, playerID: this.props.playerID}))
+            .then(responce => this.handleRoomEntered(responce))
             .catch(e => console.error(e))
+    };
+
+    handleRoomEntered = ({roomID, settings}) => {
+        const {dispatch, side} = this.props;
+        let status;
+
+        settings.roomUrl = `${window.location.origin}/?roomID=${roomID}`;
+        settings.roomID = roomID;
+
+        if (side === gameSides.server) {
+            status = gameStatuses.waitingServer;
+            window.history.pushState(null, 'RoomName', settings.roomUrl);
+        }
+
+        dispatch(setRoomSettings(settings));
+        status && dispatch(setGameStatus(status));
     };
 
     handleInputChange = event => {
@@ -74,8 +94,14 @@ class RoomCreator extends Component {
     }
 }
 
+RoomCreator.propTypes = {
+    playerID: PropTypes.string.isRequired,
+    side: PropTypes.string,
+};
+
 const mapStateToProps = state => ({
     playerID: getDeepProp(state, 'settings.playerID'),
+    side: getDeepProp(state, 'settings.side'),
 });
 
 export default connect(mapStateToProps)(RoomCreator);
