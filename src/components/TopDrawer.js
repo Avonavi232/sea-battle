38c440727915842c224489dd675fe285d0ec71d9
Avonavi232 <Avonavi232@gmail.com>
2019-04-23 22:49:17 +0300

    TopDrawer component updated

--- src/components/TopDrawer.js
+++ src/components/TopDrawer.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import styled from 'styled-components/macro';
 import {ReactComponent as SettingsIcon} from '../img/settings.svg';
 import {debounce} from "../utils/functions";
@@ -11,21 +12,9 @@ const Outer = styled.div`
   top: 0;
   transition: all .3s;
   background-color: ${({opened}) => opened ? 'rgba(0, 0, 0, .7)' : 'transparent'};
+  z-index: 1000;
 `;
 
-const Inner = styled(props => <div {...props}/>).attrs(props => ({
-    style: {
-        transform: `translateX(-50%)${props.opened ? '' : 'translateY(-100%)'}`
-    }
-}))`
-  width: 300px;
-  height: 150px;
-  background-color: #ff8787;
-  position: absolute;
-  left: 50%;
-  top: 0;
-  transition: all .3s;
-`;
 
 const Opener = styled.button`
   position: fixed;
@@ -34,6 +23,7 @@ const Opener = styled.button`
   border: none;
   background-color: transparent;
   padding: 5px;
+  z-index: 1001;
   &:focus {
     outline: none;
   }
@@ -58,32 +48,75 @@ class TopDrawer extends Component {
         this.state = {
             opened: false,
             shown: false
+        };
+
+
+        this.outerRef = React.createRef();
+        this.innerRef = React.createRef();
+
+        this.Inner = styled(this.props.Inner)`
+          position: absolute;
+          left: 50%;
+          top: 0;
+          transition: all .3s;
+        `;
+    }
+
+    static get transforms(){
+        return {
+            closed: 'translateX(-50%) translateY(-100%)',
+            opened: 'translateX(-50%) translateY(0)',
         }
     }
+
 
     toggleOpen = open => {
         this.setState({
             opened: open !== undefined ? open : !this.state.opened,
             shown: open === true || !this.state.opened === true || this.state.shown
-        });
+        }, () => this.transition(this.state.opened));
     };
 
-    toggleShown = () => {
-        if (this.state.shown) {
-            this.setState({ shown: false });
+    transition = (opened) => {
+        const
+            {current: outer} = this.outerRef,
+            {current: inner} = this.innerRef;
+
+        if (opened) {
+            outer.style.opacity = 0;
+            outer.style.display = 'block';
+            inner.style.transform = TopDrawer.transforms.closed;
+            requestAnimationFrame(() => requestAnimationFrame(() => {
+                outer.style.opacity = 1;
+                inner.style.transform = TopDrawer.transforms.opened;
+            }))
+        } else {
+            inner.style.transform = TopDrawer.transforms.closed;
         }
     };
 
+    toggleShown = () => {
+        if (!this.state.opened) {
+            this.setState({shown: false});
+        }
+    };
 
     render() {
-        const {opened, shown} = this.state;
+        const
+            {opened, shown} = this.state,
+            {Inner} = this;
 
-        return (
+        return ReactDOM.createPortal(
             <>
                 {
                     shown &&
-                    <Outer opened={opened}>
-                        <Inner opened={opened} onTransitionEnd={debounce(() => this.toggleShown(), 100)}>
+                    <Outer opened={opened} ref={this.outerRef} onClick={() => this.toggleOpen(false)}>
+                        <Inner
+                            ref={this.innerRef}
+                            opened={opened}
+                            onTransitionEnd={debounce(() => this.toggleShown(), 100)}
+                            onClick={e => e.stopPropagation()}
+                        >
                             {this.props.children}
                         </Inner>
                     </Outer>
@@ -92,7 +125,8 @@ class TopDrawer extends Component {
                 <Opener opened={opened} onClick={() => this.toggleOpen()}>
                     <SettingsIcon/>
                 </Opener>
-            </>
+            </>,
+            document.querySelector('#root')
         );
     }
 }
