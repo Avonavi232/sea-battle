7ef5ce5a8818d92222866841361c0d428300af34
Avonavi232 <Avonavi232@gmail.com>
2019-04-23 11:03:38 +0300

    soundsQueue class added

---
+++ src/utils/soundsQueue.js
@@ -0,0 +1,46 @@
+class SoundsQueue{
+    constructor() {
+        this.queue = [];
+        this.currentlyPlayed = null;
+    }
+
+    _clearAudioQueue() {
+        this.queue.length = 0;
+    }
+
+    _playSound = (audio, callback) => {
+        audio.play()
+            .catch(e => {
+                this._clearAudioQueue();
+            });
+        if (callback) {
+            audio.onended = callback;
+        }
+    };
+
+    _playSoundQueue = () => {
+        const recursive_play = () => {
+            if (this.queue.length) {
+                this.currentlyPlayed = this.queue.pop();
+                this._playSound(this.currentlyPlayed, () => recursive_play());
+            } else {
+                this.currentlyPlayed = null;
+            }
+        };
+
+        if (!this.currentlyPlayed) {
+            recursive_play();
+        }
+    };
+
+    _addToQueue = sound => {
+        this.queue.unshift(sound);
+    };
+
+    play(sound){
+        this._addToQueue(sound);
+        this._playSoundQueue();
+    }
+}
+
+export const soundsQueue = new SoundsQueue();
