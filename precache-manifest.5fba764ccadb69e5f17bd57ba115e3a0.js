8617fa5c58f2471bf97d3c5a1329fcb638211272
Avonavi232 <Avonavi232@gmail.com>
2019-04-26 19:13:47 +0300

    Updates

---
+++ precache-manifest.5fba764ccadb69e5f17bd57ba115e3a0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "932324053eb8ee0abba66941dc4bd974",
+    "url": "/sea-battle/static/media/aim.93232405.svg"
+  },
+  {
+    "revision": "e549b5eb1cf5a8c9af92",
+    "url": "/sea-battle/static/css/main.64d3585f.chunk.css"
+  },
+  {
+    "revision": "bacc1e412213acbdb6ca",
+    "url": "/sea-battle/static/js/runtime~main.db7f1d8f.js"
+  },
+  {
+    "revision": "81741f184d872c322a4ad3880cca9f13",
+    "url": "/sea-battle/static/media/unlocked.81741f18.svg"
+  },
+  {
+    "revision": "e7abdae861c69b07cadb",
+    "url": "/sea-battle/static/js/2.7dea33b7.chunk.js"
+  },
+  {
+    "revision": "82f5918b6f6afcf08cb1e6950ce6736c",
+    "url": "/sea-battle/static/media/paper.82f5918b.jpg"
+  },
+  {
+    "revision": "44983578ff189a7f3f72d7e9f20cb518",
+    "url": "/sea-battle/static/media/miss2.44983578.mp3"
+  },
+  {
+    "revision": "eb16e8d9b3a5f81545afa9e3e7a5e998",
+    "url": "/sea-battle/static/media/miss3.eb16e8d9.mp3"
+  },
+  {
+    "revision": "175ec828e01a5d8c839e531c59a9c19d",
+    "url": "/sea-battle/static/media/shipHit.175ec828.wav"
+  },
+  {
+    "revision": "e549b5eb1cf5a8c9af92",
+    "url": "/sea-battle/static/js/main.25529758.chunk.js"
+  },
+  {
+    "revision": "8cf451d2163f8aa2ab406bd68a9adad8",
+    "url": "/sea-battle/static/media/hit.8cf451d2.svg"
+  },
+  {
+    "revision": "d7451bc87bd681b9d0008c8c08010dcb",
+    "url": "/sea-battle/static/media/circle-sketch.d7451bc8.svg"
+  },
+  {
+    "revision": "8f81017c16d574e035c25a9bdcce3894",
+    "url": "/sea-battle/static/media/settings.8f81017c.svg"
+  },
+  {
+    "revision": "defe39ee10ee85465444ba31f069287f",
+    "url": "/sea-battle/static/media/volume-on.defe39ee.svg"
+  },
+  {
+    "revision": "a8c60cda515f18891dc99b85a60bc5ce",
+    "url": "/sea-battle/static/media/volume-off.a8c60cda.svg"
+  },
+  {
+    "revision": "8a6d2ca0599a1a0176bff59437cae4d2",
+    "url": "/sea-battle/static/media/locked.8a6d2ca0.svg"
+  },
+  {
+    "revision": "e7abdae861c69b07cadb",
+    "url": "/sea-battle/static/css/2.6e799397.chunk.css"
+  },
+  {
+    "revision": "55f5f8e3128fccf1c8c40d5f0d3054ff",
+    "url": "/sea-battle/index.html"
+  }
+];
